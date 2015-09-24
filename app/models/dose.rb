e5373bbb55602e79db9a89a896af9657aa1b2016
Christina Wolfe <cmw0513@gmail.com>
2015-09-24 11:54:44 +0200

    added model validations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ActiveRecord::Base
   belongs_to :cocktail
   belongs_to :ingredient
+  validates :description, presence: true
+  validates :cocktail, presence: true, :uniqueness => { :scope => :ingredient }
+  validates :ingredient, presence: true
 end

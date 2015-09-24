7adce4f18bdc31313799d2050af5199e5c042d23
Christina Wolfe <cmw0513@gmail.com>
2015-09-24 12:57:19 +0200

    added new dose feature

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,8 +1,12 @@
 class DosesController < ApplicationController
   def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredients = Ingredient.all
   end
 
   def create
+    @dose = Dose.new(dose_params)
   end
 
   def destroy

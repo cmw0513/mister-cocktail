c557371548d22c6d13c74708a2270e1ca616707a
Christina Wolfe <cmw0513@gmail.com>
2015-09-24 13:15:14 +0200

    add create dose functionality

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,9 +6,21 @@ class DosesController < ApplicationController
   end
 
   def create
-    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = @cocktail.doses.new(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :description)
   end
 end

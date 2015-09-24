a194f4a78b804e67ef35373dabb68f37376a1057
Christina Wolfe <cmw0513@gmail.com>
2015-09-24 12:07:02 +0200

    fixed route resources and added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :cocktails except: [:edit, :update, :destroy]
+  root 'cocktails#index'
+  resources :cocktails, except: [:edit, :update, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
